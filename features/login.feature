2aecd7624c57b972d650f40840a53912416785c2
Jose Waldo <josferw@gmail.com>
2025-11-11 22:39:41 -0500

    identado

--- features/login.feature
+++ features/login.feature
@@ -5,7 +5,7 @@ Feature: Para Bank Login Feature
     When I login with <username> and <password>
     Then I should see a text saying <message>
 
-    Examples: 
-      | username          | password | message           |
-      | invalidUsenam   | password | Error!            |
-      | john        | demo | Acounts Overview |
+    Examples:
+      | username      | password | message          |
+      | invalidUsenam | password | Error!           |
+      | john          | demo     | Acounts Overview |
--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,6 +1,6 @@
 import { Given, When, Then } from "@wdio/cucumber-framework";
 
-import LoginPage from '../pageobjects/login.page.js';
+import LoginPage from "../pageobjects/login.page.js";
 
 const pages = {
   login: LoginPage,
@@ -18,11 +18,11 @@ When(/^I login with (\w+) and (.+)$/, async (username, password) => {
 Then(/^I should see a text saying (.*)$/, async (message) => {
   if (message == "Error!") {
     // invalid username or password
-    await expect($('.title')).toBeExisting();
-    await expect($('.title')).toHaveTextContaining(message);
+    await expect($(".title")).toBeExisting();
+    await expect($(".title")).toHaveTextContaining(message);
   } else {
     // valid username or password
-    await expect($('.title')).toBeExisting();
-    await expect($('.title')).toHaveTextContaining(message);
+    await expect($(".title")).toBeExisting();
+    await expect($(".title")).toHaveTextContaining(message);
   }
 });
